0ca17862a6bbc7875c4adfa3295ac263c8e63aff
herrfeder <d.lassig@t-online.de>
2020-02-10 18:46:25 +0000

    Subject: Slightly changes for testing docker container

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -28,8 +28,21 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
 # Download relevant ntlk packages
-#nltk.download(["punkt", "stopwords", "wordnet"])
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
 
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download("stopwords")
+
+try:
+    nltk.data.find('corpora/wordnet')
+except LookupError:
+    nltk.download("wordnet")    
+    
 # pickle for python object serialization and storing
 import pickle
 
